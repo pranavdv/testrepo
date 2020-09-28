1b515f59149cf05ccb19f3befbd4ed1dd82ce0f3
pranavdv <71985134+pranavdv@users.noreply.github.com>
2020-09-28 19:13:01 +0530

    made changes to both files

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Heading
 
-Some descriptions
-Some more
+Contains a cpp prog to convert decimal number to binary
 
 ## subheading
-some more text
+zero is a corner case
 
 ### is this another header?
+yes. yes it is.
--- somecode.cpp
+++ somecode.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    cout<<"some code";
-    //this is a change to the code
+    int a;
+    cin>>a;
+    if(!a){
+        cout<<a;
+        return 0;
+    }
+    vector<int> bits;
+    while(a){
+        bits.push_back(a%2);
+        a>>=1;
+    }
+    for(int i=bits.size()-1;i>=0;i--) cout<<bits[i];
 }
